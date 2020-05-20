31153f7a53aa1a7481037e50ecc4a121370f437e
iinow <iinow@naver.com>
2020-05-20 09:00:15 +0900

    add zip function

---
+++ src/test/zip-test.ts
@@ -0,0 +1,8 @@
+import { zip } from '../utils'
+import { makeFakeData, IFake } from '../fake'
+
+const data = makeFakeData()
+const keys = Object.keys(data), values = Object.values(data)
+
+const fake: IFake = zip(keys, values) as IFake
+console.log(fake)
---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { getFileNameAndNumber, FileNameAndNumber } from './getFileNameAndNumber'
+import { range } from './range'
+import { zip } from './zip'
+
+export { zip, range, FileNameAndNumber, getFileNameAndNumber}
